84c3a4482c01cd0fc31b737fd36ac98ff2e9ab92
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2019-07-15 08:21:16 +0200

    Fixed install-link

--- install.ps1
+++ install.ps1
@@ -45,7 +45,7 @@ Param (
 $ModuleName = "Krbtgt"
 
 # Base path to the github repository
-$BaseUrl = "https://github.com/<InsertUsernameHere>/Krbtgt"
+$BaseUrl = "https://github.com/PSSecTools/Krbtgt"
 
 # If the module is in a subfolder of the cloned repository, specify relative path here. Empty string to skip.
 $SubFolder = "Krbtgt"
